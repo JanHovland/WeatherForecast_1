f90e7d6453e6850ec1dcee4ac2b46236bc83622d
Jan Hovland <jan.hovland@lyse.net>
2024-12-05 11:50:54 +0100

    Update

---
+++ WeatherForecast/Document Directory/loadAverageData.swift
@@ -0,0 +1,8 @@
+//
+//  loadAverageData.swift
+//  WeatherForecast
+//
+//  Created by Jan Hovland on 05/12/2024.
+//
+
+import Foundation
---
+++ WeatherForecast/Document Directory/saveAverageData.swift
@@ -0,0 +1,8 @@
+//
+//  saveAverageData.swift
+//  WeatherForecast
+//
+//  Created by Jan Hovland on 05/12/2024.
+//
+
+import Foundation
