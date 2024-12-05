cbca86b6fc2cfd14124b36da74a4911416ff5c21
Jan Hovland <jan.hovland@lyse.net>
2024-12-05 20:07:53 +0100

    Update

--- WeatherForecast/Document Directory/fileLoadFilenames.swift
+++ WeatherForecast/Document Directory/fileLoadFilenames.swift
@@ -16,10 +16,8 @@ func fileLoadFilenames() -> (LocalizedStringKey, [String]) {
     do {
         // Get the URL of the document directory
         let documentsDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        
         // Get all files in the directory
         let files = try fileManager.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil, options: [])
-        
         // Extract the file names
         fileNames = files.map { $0.lastPathComponent }
     } catch {
